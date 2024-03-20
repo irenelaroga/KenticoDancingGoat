59ab54f92cc4ac7fe2cad905e55513b3abfb63ee
irenegunning10 <iGunning@synergyonline.com>
2024-03-20 13:13:50 +0800

    initial docs

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("CMS")]
+[assembly: AssemblyProduct("CMS")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyFileVersion("11.0.6579.30664")]
+[assembly: AssemblyInformationalVersion("11.0.2")]
